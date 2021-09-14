c787501783b03aa9a536ff8e0aa66ffda6368ac8
danferh <79527228+danferh@users.noreply.github.com>
2021-09-14 05:53:58 -0600

    Initial

---
+++ HotelListing/Models/HotelDTO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListing.Models
+{
+    public class CreateHotelDTO
+    {
+        [Required]
+        [StringLength(maximumLength: 50, ErrorMessage = "Hotel name too long")]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(maximumLength: 150, ErrorMessage = "Hotel address too long")]
+        public string Address { get; set; }
+        [Required]
+        [Range(1, 5)]
+        public decimal Rating { get; set; }
+        [Required]
+        public int CountryId { get; set; }
+    }
+
+    public class HotelDTO: CreateHotelDTO
+    {
+        public int Id { get; set; }
+        public CountryDTO Country { get; set; }
+    }
+}
